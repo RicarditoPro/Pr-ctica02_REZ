d1fee7206298359942ae70b36f4094a524d3e30d
Ricardo Espejel Zarate <ricard.espejel.zarate@Outlook.com>
2023-10-18 17:46:51 -0600

    Agregar archivos de proyecto.

---
+++ APP_02_SPLASH_REZ.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Ricardo Espejel Zarate
+
+namespace APP_02_SPLASH_REZ.Droid
+{
+    [Activity(Label = "APP_REZ", Theme = "@style/SplashTheme",
+        MainLauncher = true,
+        NoHistory = true, ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.Orientation
+        )]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
